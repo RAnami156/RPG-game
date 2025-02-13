46267a482df93fb624cf33943904f0692416f09d
ANDRYEYEV Roman (123) <andryeyev123@trebesin.cz>
2025-02-13 10:09:21 +0100

    hp-bar

--- scene/world.gd
+++ scene/world.gd
@@ -3,12 +3,32 @@ extends Node2D
 @onready var animP = $AnimationPlayer
 @onready var days = $CanvasLayer/days
 @onready var time = $CanvasLayer/time
+@onready var hp_bar = $"CanvasLayer/hp-bar"
 var days_count = -1
+var time_count
 
 func _process(delta: float) -> void:
-	animP.play("day-night")
-	days.text = str(days_count) + " DAY"
+	if Global.player_healht <= 0 :
+		animP.stop()
+		hp_bar.value = Global.player_healht
+	else:
+		animP.play("day-night")
+		days.text = str(days_count) + " DAY"
+		time.text = "time: " + str(time_count)
+		hp_bar.value = Global.player_healht
 
 
 func day_plus():
 	days_count += 1 
+	
+func day():
+	time_count = "day"
+	
+func evening():
+	time_count = "evening"
+	
+func night():
+	time_count = "night"
+	
+func morning():
+	time_count = "morning" 
