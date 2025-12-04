6ec29f8006494fdf81dfccd0409f4802bfb2aba5
RAnami156 <romaandreev414@gmail.com>
2025-12-04 21:30:18 +0100

    boss 0.1

--- script/area_2d.gd
+++ script/area_2d.gd
@@ -4,16 +4,20 @@ extends Area2D
 var player_in = false
 
 func _physics_process(delta: float) -> void:
-	if player_in:
-		text.visible = true
-	else:
-		text.visible = false
-	if player_in and Global.slime_killed >= 2:
-		text.text = "Press E to fight boss"
-	else:
-		text.text = "Kill 10 slides to fight the boss"
-	if player_in and Global.slime_killed >= 2 and Input.is_action_just_pressed("E"):
+	if player_in and Input.is_action_just_pressed("E"):
+		Global.player_position = Vector2(578,331)
 		get_tree().change_scene_to_file("res://scene/boss_level.tscn")
+	
+	#if player_in:
+		#text.visible = true
+	#else:
+		#text.visible = false
+	#if player_in and Global.slime_killed >= 2:
+		#text.text = "Press E to fight boss"
+	#else:
+		#text.text = "Kill 10 slides to fight the boss"
+	#if player_in and Global.slime_killed >= 2 and Input.is_action_just_pressed("E"):
+		#get_tree().change_scene_to_file("res://scene/boss_level.tscn")
 
 
 func _on_body_entered(body: Node2D) -> void:
