01ffb478fd99ba4d80996fe7d45ac9f87d06122a
Roman <roman@Romans-MacBook-Air.local>
2026-05-23 21:30:16 +0200

    add music and sound

--- script/money.gd
+++ script/money.gd
@@ -8,8 +8,10 @@ func _ready() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player" :
+		$coin_sound.play() 
 		Global.player_money += 1
 		animP.play("up")
 		await animP.animation_finished
+		$coin_sound.stop()
 		queue_free()
 		print(Global.player_money)
